33e90925d778255c658ad156fb2eaa30f461ddac
Ak-ko <waterbubbles.ww6615@gmail.com>
2024-05-31 16:14:41 +0800

    blog-bug-fix

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,17 +3,24 @@
 namespace App\Models;
 
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Blog extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public static function getForm()
     {
@@ -25,12 +32,12 @@ class Blog extends Model
                         ->label('Blog Photo')
                         ->required()
                         ->maxSize(1024 * 1024 * 2)
-                        ->rules('required')
                         ->disk('public')
                         ->visibility('public')
                         ->directory('blogphotos')
                         ->image()
-                        ->preserveFilenames(),
+                        ->preserveFilenames()
+                        ->rules('required'),
                     TextInput::make('title')
                         ->required()
                         ->rules('required|string')
@@ -39,6 +46,8 @@ class Blog extends Model
                         ->required()
                         ->rules('required|string')
                         ->columnSpanFull(),
+                    Hidden::make('user_id')
+                        ->default(auth()->user()->id)
                 ])
         ];
     }
