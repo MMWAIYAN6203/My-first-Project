979488768fc512cfde8663911c462decf11786ae
Ak-ko <waterbubbles.ww6615@gmail.com>
2024-05-31 16:04:04 +0800

    blog-crud-added

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,6 +2,10 @@
 
 namespace App\Models;
 
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +14,32 @@ class Blog extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public static function getForm()
+    {
+        return [
+            Section::make('Blog publish')
+                ->description('You can create a blog here')
+                ->schema([
+                    FileUpload::make('photo')
+                        ->label('Blog Photo')
+                        ->required()
+                        ->maxSize(1024 * 1024 * 2)
+                        ->rules('required')
+                        ->disk('public')
+                        ->visibility('public')
+                        ->directory('blogphotos')
+                        ->image()
+                        ->preserveFilenames(),
+                    TextInput::make('title')
+                        ->required()
+                        ->rules('required|string')
+                        ->maxLength(255),
+                    RichEditor::make('description')
+                        ->required()
+                        ->rules('required|string')
+                        ->columnSpanFull(),
+                ])
+        ];
+    }
 }
