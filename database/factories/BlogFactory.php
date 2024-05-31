7502551dd406102b302b8499cf0c81b364f72498
Ak-ko <waterbubbles.ww6615@gmail.com>
2024-05-31 15:48:24 +0800

    blog-migration-added

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->title(),
+            'description' => fake()->paragraph(4),
+            'photo' => fake()->imageUrl(),
+            'user_id' => 1,
+        ];
+    }
+}
