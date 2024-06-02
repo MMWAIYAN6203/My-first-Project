62000dbce6113906abe07d0a3aab35f40dfcf3db
Ak-ko <waterbubbles.ww6615@gmail.com>
2024-06-02 17:46:49 +0800

    done

--- resources/views/blog_detail.blade.php
+++ resources/views/blog_detail.blade.php
@@ -9,8 +9,9 @@
         </h1>
         <img class="object-fill rounded-xl shadow-2xl w-[40vh] h-[60vh] sm:w-[60vh] sm:h-[80vh] md:w-[70vh] md:h-[90vh] lg:w-[70vh] lg:h-[90vh] xl:w-[60vh] xl:h-[80vh]"
             src="{{ asset("storage/$blog->photo") }}" alt="image of blog">
-        <p class="text-[10px] sm:text-[14px] md:text-[14px] lg:text-[16px] xl:text-lg py-[40px] ">
+        <div class="p-4 my-5 text-base leading-8 no-tailwindcss-base">
             {!! $blog->description !!}
+        </div>
         </p>
     </div>
     <script src="/script.js"></script>
